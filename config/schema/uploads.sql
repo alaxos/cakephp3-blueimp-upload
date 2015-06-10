a90159fca60cd80f514071ffd14710c2bae5157d
Nicolas Rod <nico@alaxos.com>
2015-06-10 16:58:31 +0200

    new SQL file to create the 'uploads' table

---
+++ config/schema/uploads.sql
@@ -0,0 +1,16 @@
+CREATE TABLE IF NOT EXISTS `uploads` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `original_filename` varchar(256) CHARACTER SET utf8 NOT NULL,
+  `unique_filename` varchar(256) CHARACTER SET utf8 NOT NULL,
+  `subfolder` varchar(50) CHARACTER SET utf8 DEFAULT NULL,
+  `mimetype` varchar(50) CHARACTER SET utf8 DEFAULT NULL,
+  `size` int(11) NOT NULL,
+  `hash` varchar(256) CHARACTER SET utf8 NOT NULL,
+  `upload_id` varchar(100) CHARACTER SET utf8 NOT NULL,
+  `complete` tinyint(1) NOT NULL,
+  `label` varchar(256) CHARACTER SET utf8 DEFAULT NULL,
+  `created` datetime NOT NULL,
+  `modified` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `upload_id` (`upload_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
